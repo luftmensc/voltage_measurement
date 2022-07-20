6303a6e5c9459e8ec997abfe9f38940840242853
Ömer Faruk Okuyan <99191745+luftmensc@users.noreply.github.com>
2022-07-20 17:14:57 +0300

    Add files via upload

---
+++ sketch_jul15a/sketch_jul15a.ino
@@ -0,0 +1,28 @@
+float resistor_cons = 5.6;
+float arduino_cons = 0.0048875855327468;
+#include <SoftwareSerial.h> 
+
+SoftwareSerial Serial2(2,3);
+  
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+ Serial2.begin(9600);
+ pinMode(A0, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  String str;
+  int pinData = analogRead(A0);
+  float R_volt =(pinData* arduino_cons);
+  float base = (R_volt*resistor_cons);
+  str=String(base);
+  
+  Serial2.print("t0.txt=\""+str+"\"");
+  Serial2.write(0xff);
+  Serial2.write(0xff);
+  Serial2.write(0xff);
+  delay(500);
+
+}
